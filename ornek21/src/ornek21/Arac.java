e69116535043807a26cf1d046b274b174e35f26b
mfadak <fatih@returnsoft.com.tr>
2023-12-29 21:51:19 +0300

    ilk yukleme

---
+++ ornek21/src/ornek21/Arac.java
@@ -0,0 +1,20 @@
+package ornek21;
+
+public class Arac {
+	private double hiz;
+	private double yol;
+	
+	public Arac() {
+		hiz=0;
+		yol=0;
+	}
+	public void hizlan(double hiz) {
+		this.hiz = hiz;
+	}
+	public void sur(int saat) {
+		yol = hiz*saat;
+	}
+	public double getYol() {
+		return yol;
+	}
+}
